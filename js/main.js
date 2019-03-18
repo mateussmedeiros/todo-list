8397a754609f9119c82db8a5c8b8d8101618e8ff
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-03-18 19:12:20 -0300

    Adicionando função para criar itens

--- js/main.js
+++ js/main.js
@@ -10,9 +10,32 @@ function montarPagina() {
     
     // Botão inserir tarefa
     var btnToDo = document.createElement('button');
+    btnToDo.setAttribute('onclick','novoItem()');
     btnToDo.innerHTML = 'Add';
     
-    // Inserir o input e botão dentro da div
+    // Lista de tarefas
+    var list = document.createElement('ul');
+    list.setAttribute('id', 'list');
+    
+    // Inserir o input, botão e lista dentro da div
     app.appendChild(iptToDo);
     app.appendChild(btnToDo);
+    app.appendChild(list);
+}
+
+// Adicionar novo item
+function novoItem() {
+    // Pegar ul e criar li
+    var list = document.getElementById('list');
+    var li = document.createElement('li');
+    
+    // Pegar valor inserido
+    var item = document.getElementById('todo').value;
+    
+    // Adicionar valor na lista
+    li.appendChild(document.createTextNode(item));
+    list.appendChild(li);
+    
+    // Reseta valor do input
+    document.getElementById('todo').value = "";
 }
