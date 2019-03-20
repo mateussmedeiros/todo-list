ca89e230ee35a766f1423ee8f563fb7af2ca61f3
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-03-20 12:17:04 -0300

    Adicionando ícone de exclusão ao inserir tarefa na lista

--- js/main.js
+++ js/main.js
@@ -28,13 +28,25 @@ function novoItem() {
     // Pegar ul e criar li
     var list = document.getElementById('list');
     var li = document.createElement('li');
+
+    // Ícone de exclusão
+    var trash = document.createElement('button');
+    trash.setAttribute('id', 'trash');
+    trash.innerHTML = 'X';
     
     // Pegar valor inserido
     var item = document.getElementById('todo').value;
     
-    // Adicionar valor na lista
-    li.appendChild(document.createTextNode(item));
-    list.appendChild(li);
+    // Verificar se o campo foi preenchido
+    // Se sim, adiciona valor na lista
+    // Se não, exibe uma mensagem de erro
+    if(item.length >= 1) {
+        li.appendChild(document.createTextNode(item));
+        li.appendChild(trash);
+        list.appendChild(li);
+    } else {
+        alert('O campo não foi preenchido');
+    }
     
     // Reseta valor do input
     document.getElementById('todo').value = "";
